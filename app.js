6cf054f52b106fc5b0c76621adf5caccabd7ab08
mopelola1ARIKE <abionamopelola@gmail.com>
2022-04-15 07:59:46 -0400

    javascript 1

---
+++ app.js
@@ -0,0 +1,23 @@
+var num1=prompt("please enter num 1");
+
+var num2=prompt("please enter num2");
+
+var total=num1+num2;
+
+alert(total)
+
+var firstname=prompt("Enter your firstname");
+
+var first=firstname.slice(0,4);
+
+var firstn=first.toLowerCase();
+
+var lastname=prompt("Enter your lastname");
+
+var last=lastname.slice(0,1);
+
+var lastn=last.toLowerCase();
+
+var userid=" your capacityBay userid is " + firstn + lastn;
+
+alert(userid)
